9a580df43052085807a4fcc3c69ced495c4c9fe4
Toni Pesola <Toni@Tonis-MacBook-Pro.local>
2014-10-28 13:30:52 +0200

    Resources are now defined in data/resourceManager.xml file

--- o1/adventure/render/ResourceManager.scala
+++ o1/adventure/render/ResourceManager.scala
@@ -8,27 +8,53 @@ import java.io.File
 import java.awt.image.BufferedImage
 import o1.event.Event
 import o1.event.EventType._
+import scala.xml.XML
 
 object ResourceManager {
   val meshes = Map[String, Mesh]()
   val shapes = Map[String, Shape]()
   val images = Map[String, BufferedImage]()
   val strings = Map[String, String]()
-  
-  meshes("sphere") = Mesh("data/sphere.obj")
-  meshes("monkey") = Mesh("data/monkey.obj")
-  meshes("cube") = Mesh("data/cube.obj")
-  meshes("plate") = Mesh("data/4x4_plate_floor.obj")
-  meshes("floor") = Mesh("data/floor.obj")
-  meshes("floor").luminosity = 0.6f
-  meshes("testMap") = Mesh("data/maps/00_testMap.obj")
-  meshes("testMap").luminosity = 1.0f
 
-  images("cross") = ImageIO.read(new File("data/logo.png"))
-  strings("helpMenu") = "This text should wrap nicely. " +
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-    "Morbi placerat dui nec cursus bibendum. Integer auctor, " +
-    "dui gravida semper maximus, arcu massa semper libero, " +
-    "vitae laoreet nulla nulla non leo.\nThis should be alone on this line."
-   
+  val xml = XML.loadFile("data/resourceManager.xml")
+  val XMLmeshes = xml \ "meshes" \ "item"
+  val XMLimages = xml \ "images" \ "item"
+  val XMLstrings = xml \ "strings" \ "item"
+
+  for (m <- XMLmeshes) {
+    val name = (m \ "@name")
+    val path = (m \ "@path")
+    println("Loaded mesh: " + name + ", " + path)
+    meshes(name.text) = Mesh(path.text)
+  }
+  
+  for (m <- XMLimages) {
+    val name = (m \ "@name")
+    val path = (m \ "@path")
+    println("Loaded image: " + name + ", " + path)
+    images(name.text) = ImageIO.read(new File(path.text))
+  }
+  
+  for (m <- XMLstrings) {
+    val name = (m \ "@name")
+    val str = (m \ "string")
+    println("Loaded string: " + name)
+    strings(name.text) = str.text
+  }
+  
+  //  meshes("sphere") = Mesh("data/sphere.obj")
+  //  meshes("monkey") = Mesh("data/monkey.obj")
+  //  meshes("cube") = Mesh("data/cube.obj")
+  //  meshes("plate") = Mesh("data/4x4_plate_floor.obj")
+  //  meshes("floor") = Mesh("data/floor.obj")
+  //  meshes("floor").luminosity = 0.6f
+  //  meshes("testMap") = Mesh("data/maps/00_testMap.obj")
+  //  meshes("testMap").luminosity = 1.0f
+  //
+  //  images("cross") = ImageIO.read(new File("data/logo.png"))
+  //  strings("helpMenu") = "This text should wrap nicely. " +
+  //    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+  //    "Morbi placerat dui nec cursus bibendum. Integer auctor, " +
+  //    "dui gravida semper maximus, arcu massa semper libero, " +
+  //    "vitae laoreet nulla nulla non leo.\nThis should be alone on this line."  
 }
--- o1/inventory/InventoryTestApp.scala
+++ o1/inventory/InventoryTestApp.scala
@@ -1,33 +1,29 @@
 package o1.inventory
 
 import o1.adventure.render.ResourceManager
+import scala.xml._
+import java.io.File
 
-object InventoryTestApp extends App{
-  val inv = Inventory
-  println("## " + inv.toString())
-  
-  val page1 = Page("Page1","This is a page1")
-  val page2 = Page("Page2","This is a page2")
-  val page3 = Page("Page3","This is a page3")
-  val page4 = Page("Page4","This is a page4")
-  val page5 = Page()
-  
-  val c1 = Coffee()
-  val c2 = Page("Page2","This is a page2")
-  val c3 = Page("Page3","This is a page3")
-  val c4 = Page("Page4","This is a page4")
-  val c5 = Page()
-  
-  inv.addItem(page1)
-  inv.addItem(page2)
-  inv.addItem(page3)
-  inv.addItem(page4)
-  println("## " + inv.toString())
-  inv.removeItem(page1)
-  inv.removeItem(page2)
-  println("## " + inv.toString())
-  inv.removeItem(page5)
-  println("## " + inv.toString())
-  inv.removeOneOfType(Page())
-  println("## " + inv.toString())
+object InventoryTestApp extends App {
+
+  val xml = XML.loadFile("data/resourceManager.xml")
+  val meshes = xml \ "meshes" \ "item"
+  val images = xml \ "images" \ "item"
+  val strings = xml \ "strings" \ "item"
+  println("Meshes lenght: " + meshes.length)
+  println("Images lenght: " + images.length)
+  println("Strings lenght: " + strings.length)
+  for (mesh <- meshes) {
+    println("Mesh: " + mesh \ "@name" + ", with path " + mesh \ "@path")
+  }
+  for (image <- images) {
+    println("Image: " + image \ "@name" + ", with path " + image \ "@path")
+  }
+  for (string <- strings) {
+    println("String: " + string \ "@name" + " with text:")
+    val text = (string \ "string").text
+    println(text)
+    
+  }
+
 }
