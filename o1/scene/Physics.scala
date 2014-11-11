91a798ae3bf0eba6133a6984025e4bdc9c4539bf
Juuso Toikka <jtoikka@gmail.com>
2014-11-11 14:55:35 +0200

    Coffee physics

---
+++ o1/scene/Physics.scala
@@ -0,0 +1,11 @@
+package o1.scene
+
+object Physics {
+  def applyPhysics(entity: Entity, delta: Double) = {
+    val spatial = entity.getComponent(SpatialComponent.id).get
+    val physics = entity.getComponent(PhysicsComponent.id).get
+    
+    spatial.position += physics.velocity * delta.toFloat
+    physics.velocity += physics.acceleration * delta.toFloat
+  }
+}
