d040f76e6e75eec80455d7d96fd3c11e7d033688
Toni Pesola <pesola.toni@gmail.com>
2014-11-15 23:11:18 +0200

    Added keys

---
+++ o1/inventory/Key.scala
@@ -0,0 +1,17 @@
+package o1.inventory
+
+import o1.adventure.render.ResourceManager
+import o1.adventure.render2D.Image2D
+import java.io.File
+import javax.imageio.ImageIO
+
+object Key{
+  val defaultName = "Key" // Should be in resourceManager
+  val defaultDesc = "Key Desc"
+  def apply(n: String,d:String ) = new Key(n,d)
+  def apply() = new Key(defaultName,defaultDesc)
+}
+
+class Key(name: String, desc: String) extends Item(name, desc){ 
+  var icon = "icon_key"
+}
