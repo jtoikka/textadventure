529f7a5f7aae4edd61640dbbc2026b8b4f78c415
Juuso Toikka <jtoikka@gmail.com>
2014-11-25 16:04:58 +0200

    Lab fix

--- o1/adventure/render/Texture.scala
+++ o1/adventure/render/Texture.scala
@@ -7,6 +7,7 @@ import java.io.File
 class Texture(filePath: String) {
   val image = ImageIO.read(new File(filePath))
   
+  println(filePath)
   private val _width = image.getWidth()
   private val _height = image.getHeight()
   
